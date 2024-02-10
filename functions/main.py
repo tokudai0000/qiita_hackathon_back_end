36e418aee552ff28a3d1ededaf83d3f141ff96d6
hika138 <hikax138@gmail.com>
2024-02-10 15:41:14 +0900

    デプロイ成功

--- functions/main.py
+++ functions/main.py
@@ -1,11 +1,13 @@
 # The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-from firebase_functions import firestore_fn, https_fn
+from firebase_functions import firestore_fn, https_fn, options
 
 # The Firebase Admin SDK to access Cloud Firestore.
 from firebase_admin import initialize_app, firestore
 import google.cloud.firestore
 
 app = initialize_app()
+options.set_global_options(region=options.SupportedRegion.ASIA_NORTHEAST1)
+
 @https_fn.on_request()
 def addmessage(req: https_fn.Request) -> https_fn.Response:
     """Take the text parameter passed to this HTTP endpoint and insert it into
