888c51c1ce252ab24cbcb373cb2d49c27c51c937
hika138 <hikax138@gmail.com>
2024-02-10 19:41:12 +0900

    動きません。悲しい。

--- functions/main.py
+++ functions/main.py
@@ -1,9 +1,36 @@
-# The Cloud Functions for Firebase SDK to create Cloud Functions and set up triggers.
-from firebase_functions import firestore_fn, https_fn, options
-
-# The Firebase Admin SDK to access Cloud Firestore.
-from firebase_admin import initialize_app, firestore
+import datetime
+import zoneinfo
 import google.cloud.firestore
+import firebase_admin
+from firebase_admin import firestore, initialize_app
+from firebase_functions import firestore_fn, https_fn
 
+# Firebase Admin SDKの初期化
 app = initialize_app()
-options.set_global_options(region=options.SupportedRegion.ASIA_NORTHEAST1)
+
+# Firestoreクライアントの取得
+db = firestore.client()
+
+collection = "userdata"
+
+@https_fn.on_request()
+def UserJoin(request):
+    # リクエストボディからJSONを取得
+    request_json = request.get_json()
+
+    # JSONをPythonの配列に変換
+    data_array = []
+    for item in request_json:
+        data_array.append({ 
+            "UUID": item["UUID"],
+            "lang": item["lang"],
+        }
+    )
+
+    # Firestoreにデータを格納
+    collection_ref = db.collection(collection)
+    document_ref = collection_ref.document(data_array["UUID"])
+    document_ref.set({"data": data_array})
+
+    # レスポンスを返す
+    return "OK"
